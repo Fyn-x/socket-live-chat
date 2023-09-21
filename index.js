28f47f0876acb759e2cf92100aa71eebd4ddfaa7
Fyn-x <60082813+Fyn-x@users.noreply.github.com>
2023-09-21 10:11:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8888;
+
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io')
+const io = new Server(server, {cors: {origin: "*"}})
+
+const users = {};
+io.on('connection', (socket) => {
+  socket.on('client-join', ({ id }) => {
+    users[id] = socket.id;
+    console.log('masuk')
+    console.log(users)
+  });
+
+  socket.on('disconnect', function() {
+    console.log('Got disconnect!');
+    for (const key in users) {
+      if (users[key] === socket.id) {
+        delete users[key];
+        break;
+      }
+    }
+    
+    console.log(users);
+  });
+
+  socket.on('pesan-client', (data) => {
+    if (users[data.id]) {
+      socket.to(users['admin']).emit('client-to-admin', data);
+    } 
+  });
+
+  socket.on('admin-to-client', (data) => {
+    socket.to(users[data.client_id]).emit('server-to-client', data.pesan)
+  });
+});
+
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
